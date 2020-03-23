8d2cc20565ffba81423a1502a93a88a55ae82fbd
michal <matiamic@fel.cvut.cz>
2020-03-23 05:40:12 +0100

    ubuntu bugfix

--- install_env.sh
+++ install_env.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 pip3 install virtualenv
 virtualenv calgen_env
+if [ ! -d calgen_env ]
+then
+    echo 'PROBLEM PRI INSTALACI virtualenv'
+    exit 1
+fi
 source calgen_env/bin/activate
 pip install ics requests
 deactivate
